3f538f1afb0bc913752bb894369e03624b858e0e
unknown <aniketgoyal791@gmail.com>
2024-12-05 14:27:31 +0530

    day 14

--- week 2/day 14.py
+++ week 2/day 14.py
@@ -3,3 +3,29 @@
 # Given a string s, return the longest palindromic substring in s.
 
 
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ss=s[0]
+
+        for i in range(len(s)):
+            l,r=i,i
+            while r<len(s) and l>=0 and s[l]==s[r]:
+                if(r-l+1>len(ss)):
+                    ss=s[l:r+1]
+                r+=1
+                l-=1
+
+            l,r=i,i+1
+            while r<len(s) and l>=0 and s[l]==s[r]:
+                if(r-l+1>len(ss)):
+                    ss=s[l:r+1]
+                r+=1
+                l-=1
+        if(not ss and len(s)==1):
+            return s
+        return ss
+                    
