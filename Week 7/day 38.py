6aa548d02a5b0bdb389a5a1c3300414cb32f3e50
unknown <aniketgoyal791@gmail.com>
2024-12-09 16:03:09 +0530

    day 38

---
+++ Week 7/day 38.py
@@ -0,0 +1,30 @@
+# 134. Gas Station
+
+# There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+# You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+# Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique.
+
+
+
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """ 
+        if(sum(gas)<sum(cost)):
+            return -1
+        
+        start=0
+        total=0
+
+        for i in range(len(cost)):
+            total+=gas[i]-cost[i]
+
+            if(total<0):
+                start=i+1
+                total=0
+        return start
